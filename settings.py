1b20660a9895f7926ee1b33f9bfef3bb3c470f4a
Niriktelbot <110273551+Niriktelbot@users.noreply.github.com>
2022-11-29 20:25:48 +0600

    Add files via upload

---
+++ settings.py
@@ -0,0 +1,16 @@
+token = "5817061510:AAE2OH2UuvZXJZHhrao8SY50FpsGrNTGoNQ"
+botname = 'AkatsukiiSuberBot'
+channel_id = -1001886783842 # айди канала
+link_channel = "@indeksqiwi" # сылка на канал
+
+QIWI_ACCOUNT = '996705263660' # Номер кошелька без 
+p2p_qiwi_public_key = "48e7qUxn9T7RyYE1MVZswX1FRSbE6iyCj2gCRwwF3Dnh5XrasNTx3BGPiMsyXQFNKQhvukniQG8RTVhYm3iPqq5T8Xch4S57nh3CLDm3ZGeCtqw6ChJogppGH66nSHnMWjEozQusr1WR9VD5fEyi19wRuEKUKoCyNj9qaKmMpFkjtnJa5oxGLmmxDVExF"
+p2p_qiwi_secret_key = "=eyJ2ZXJzaW9uIjoiUDJQIiwiZGF0YSI6eyJwYXlpbl9tZXJjaGFudF9zaXRlX3VpZCI6ImRoOXcxMy0wMCIsInVzZXJfaWQiOiI5OTY3MDUyNjM2NjAiLCJzZWNyZXQiOiI5MWViZTQzMTMyMTFlNmQ3MWJmYmQ5YjM2NDcyOTY2NzBkNTg4N2E1NmM1ZmQ5NDQ1NzBkYjIxNWE1YTRjMDA2In19"
+admins = [5504563793] 
+
+LITTLE_SUBCOIN_TO_GET_SUBS = 5 #минимум сабкоинов для раскрутки канала
+ref_count = 2 #реферальный бонус (сабкоины)
+REF_BONUS = 2
+FINE_FOR_UNSUBSCRIBING = -5 #Число должно быть отрицательным и целым
+SUBSCRIPTION_TERM = 7 #Какое время юзер должен оьязательно быть подписан на канал?
+LINK_TO_INTRODUCTION_AND_RULES = 'https://telegra.ph/Vvedenie-v-bota-a-takzhe-pravila-01-16' #ссылка на правила и инструкцию
